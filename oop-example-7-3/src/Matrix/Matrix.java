cdf899b5ea27d23acc6adbc3b2fc80a412fffb14
sderevianko <derevianko.serhii@gmail.com>
2020-05-19 21:29:00 +0300

    Corrected small issue: get & set method

--- oop-example-7-3/src/Matrix/Matrix.java
+++ oop-example-7-3/src/Matrix/Matrix.java
@@ -1,11 +1,52 @@
 package Matrix;
 
 public class Matrix {
-    public int [][] array1 = {{1,2,3}, {3,4,5}, {5,6,7}};
-    public int [][] array2 = {{10,0,17}, {3,4,5}, {111,32,9}};
-    public int [][] array3 = {{0,0,0}, {0,0,0}, {0,0,0}};;
-    public int [][] array4 = {{0,0,0}, {0,0,0}, {0,0,0}};
-    public int [][] array5 = {{0,0,0}, {0,0,0}, {0,0,0}};
+    final int ARRAY_DIMENSION = 3;
+    private int[][] array1;
+    private int[][] array2;
+    private int[][] array3;
+    private int[][] array4;
+    private int[][] array5;
+
+    public int[][] getArray1() {
+        return array1;
+    }
+
+    public int[][] getArray2() {
+        return array2;
+    }
+
+    public int[][] getArray3() {
+        return array3;
+    }
+
+    public int[][] getArray4() {
+        return array4;
+    }
+
+    public int[][] getArray5() {
+        return array5;
+    }
+
+    public void setArray1(int[][] array1) {
+        this.array1 = array1;
+    }
+
+    public void setArray2(int[][] array2) {
+        this.array2 = array2;
+    }
+
+    public void setArray3(int[][] array3) {
+        this.array3 = array3;
+    }
+
+    public void setArray4(int[][] array4) {
+        this.array4 = array4;
+    }
+
+    public void setArray5(int[][] array5) {
+        this.array5 = array5;
+    }
 
     public void arrayAddition() {
         System.out.println();
@@ -17,32 +58,33 @@ public class Matrix {
                 System.out.print(" ");
             }
         }
-
     }
+
     public void arrayMultiplyByNumber() {
         System.out.println();
         System.out.print("array 1 multiply by number 5 = array4");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < ARRAY_DIMENSION; i++) {
             System.out.println();
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < ARRAY_DIMENSION; j++) {
                 System.out.print(array4[i][j] = array1[i][j] * 5);
                 System.out.print(" ");
             }
         }
     }
+
     public void arrayMultiplyByArray() {
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                for(int w = 0; w < 3; w++) {
+        for(int i = 0; i < ARRAY_DIMENSION; i++) {
+            for(int j = 0; j < ARRAY_DIMENSION; j++) {
+                for(int w = 0; w < ARRAY_DIMENSION; w++) {
                     array5[i][j] += array1[i][w] * array2[w][j];
                 }
             }
         }
         System.out.println();
         System.out.print("array1 multiply by array2 = array5");
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < ARRAY_DIMENSION; i++) {
             System.out.println();
-            for(int j = 0; j < 3; j++) {
+            for(int j = 0; j < ARRAY_DIMENSION; j++) {
                 System.out.print(array5[i][j]);
                 System.out.print(" ");
             }
--- oop-example-7-3/src/application/Main.java
+++ oop-example-7-3/src/application/Main.java
@@ -6,31 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[][] arrayFirst = {{1,2,3}, {3,2,1}, {1,2,3}};
+        int[][] arraySecond = {{9,8,7}, {7,8,9}, {9,8,7}};
+        int[][] arrayNull = {{0,0,0}, {0,0,0}, {0,0,0}};
 
-        int [][] array1 = {{1,2,3}, {3,4,5}, {5,6,7}};
-        int [][] array2 = {{10,0,17}, {3,4,5}, {111,32,9}};
-        int [][] array3 = {{0,0,0}, {0,0,0}, {0,0,0}};
-        int [][] array4 = {{0,0,0}, {0,0,0}, {0,0,0}};
-        int [][] array5 = {{0,0,0}, {0,0,0}, {0,0,0}};
+        Matrix matrix = new Matrix();
+        matrix.setArray1(arrayFirst);
+        matrix.setArray2(arraySecond);
+        matrix.setArray3(arrayNull);
+        matrix.setArray4(arrayNull);
+        matrix.setArray5(arrayNull);
 
-        Matrix a = new Matrix();
-        System.out.print("array1");
-        for (int i = 0; i < 3; i++) {
-            System.out.println();
-            for (int j = 0; j < 3; j++) {
-                System.out.print(array1[i][j] + " ");
-            }
-        }
-        System.out.println();
-        System.out.print("array2");
-        for (int i = 0; i < 3; i++) {
-            System.out.println();
-            for (int j = 0; j < 3; j++) {
-                System.out.print(array2[i][j]+ " ");
-            }
-        }
-        a.arrayAddition();
-        a.arrayMultiplyByNumber();
-        a.arrayMultiplyByArray();
+        matrix.arrayAddition();
+        matrix.arrayMultiplyByNumber();
+        matrix.arrayMultiplyByArray();
     }
 }
